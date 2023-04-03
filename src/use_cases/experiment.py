3855fb90c095723e36212279eabe512367fdc5fc
GwonilJoo <gwonil3256@gmail.com>
2023-04-03 09:31:15 +0900

    [FEAT] convert docker to kubernetes in usecase

--- src/use_cases/experiment.py
+++ src/use_cases/experiment.py
@@ -5,52 +5,67 @@ import os
 from src.domain.experiment import Experiment
 from src.requests.experiment import ExperimentRequest, ExperimentListRequest
 
+JOB_TEMPLATE = \
+"""cat << EOF | kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: ml-epoch-%02d-lr-%.3f-bs-%03d
+spec:
+  template:
+    spec:
+      containers:
+        - name: ml
+          image: gwonil/ml:latest
+          resources:
+            requests:
+              memory: "4000Mi"
+          env:
+            - name: LEARNING_RATE
+              value: "%s"
+            - name: EPOCHS
+              value: "%s"
+            - name: BATCH_SIZE
+              value: "%s"
+            - name: EXPERIMENT_ID
+              value: "exp%02d"
+            - name: DB_HOST
+              value: "mongo"
+            - name: DB_PORT
+              value: "27017"
+            - name: SAVE_DIR
+              value: "/saved"
+            - name: MNIST_DIR
+              value: "/saved/MNIST_data/"
+            - name: PYTHONUNBUFFERED
+              value: "0"
+          volumeMounts:
+            - name: varlog
+              mountPath: "/saved"
+      volumes:
+        - name: varlog
+          hostPath:
+            path: "/Users/gwonil/Documents/kubernetes-mlops/saved"
+      restartPolicy: Never
+  backoffLimit: 0
+EOF
+"""
 
 class ExperimentUseCase:
     def __init__(self):
-        # self.JOB_TEMPLATE = \
-        #     """cat << EOF | kubectl apply -f -
-        #     apiVersion: batch/v1
-        #     kind: Job
-        #     metadata:
-        #     name: exp%02d
-        #     spec:
-        #     template:
-        #         spec:
-        #         containers:
-        #         - name: ml
-        #             image: %s
-        #             command: ["python", "train.py"]
-        #             args: ['%s', '%s', '%s']
-        #             resources:
-        #             limits:
-        #                 cpu: "1"
-        #                 memory: "6Gi"
-        #         restartPolicy: Never
-        #     EOF
-        #     """
-        
-        self.JOB_TEMPLATE = ""
-        self.JOB_TEMPLATE += "docker run -itd --rm "
-        self.JOB_TEMPLATE += "-e LEARNING_RATE=%s "
-        self.JOB_TEMPLATE += "-e EPOCHS=%s "
-        self.JOB_TEMPLATE += "-e BATCH_SIZE=%s "
-        self.JOB_TEMPLATE += "-e EXPERIMENT_ID=%s "
-        self.JOB_TEMPLATE += "--network %s "
-        self.JOB_TEMPLATE += "--name ml-%s "
-        self.JOB_TEMPLATE += "ml:latest"
-
+        self.JOB_TEMPLATE = JOB_TEMPLATE
 
     def start_experiment(self, experiments: List[ExperimentRequest]) -> bool:
         try:
             for i, exp in enumerate(experiments):
                 args = [
-                    exp.learngin_rate,
                     exp.epochs,
+                    exp.learning_rate,
                     exp.batch_size,
-                    str(uuid4()),
-                    "docker-env_default",
-                    i,
+                    str(exp.learning_rate),
+                    str(exp.epochs),
+                    str(exp.batch_size),
+                    i+1,
                 ]
                 run_job_cmd = self.JOB_TEMPLATE % tuple(args)
                 os.system(run_job_cmd)
